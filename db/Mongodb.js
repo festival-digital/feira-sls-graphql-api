df66691ed039efd131d6603e3e1bd5e46c9ce816
Isaque Costa <isaquebc@gmail.com>
2020-08-07 13:15:41 -0300

    Fixing controllers import

--- db/Mongodb.js
+++ db/Mongodb.js
@@ -1,28 +1,9 @@
 import mongoose from 'mongoose';
 import users from './schemas/users.model';
-import phones from './schemas/phones.model';
-import adresses from './schemas/adresses.model';
-import sneakers from './schemas/sneakers.model';
-import cards from './schemas/cards.model';
-import adComplaints from './schemas/adComplaintsModel.model';
-import ads from './schemas/ads.model';
-import orders from './schemas/orders.model';
-import bankAccounts from './schemas/bankAccounts.model';
-import multiOrders from './schemas/multiOrders.model';
-import offers from './schemas/offers.model';
-import configurations from './schemas/configurations.model';
-import payments from './schemas/payments.model';
-import transactions from './schemas/transactions.model';
-import comments from './schemas/comments.model';
-import banks from './schemas/bank.model';
-import tickets from './schemas/tickets.model';
-import countries from './schemas/countries.model';
-import newsletterUsers from './schemas/newsletterUsers.model';
-import errors from './schemas/errors.model';
 
 mongoose.Promise = global.Promise;
 
-export default async ({ conn, mongoUrl = 'mongodb://localhost/aceOne-local' }) => {
+export default async ({ conn, mongoUrl = 'mongodb://localhost/feira-digital' }) => {
   console.log('mongoUrl: ', mongoUrl);
   try {
     if (!conn) {
@@ -35,26 +16,6 @@ export default async ({ conn, mongoUrl = 'mongodb://localhost/aceOne-local' }) =
       });
 
       newConnection.model('users', users);
-      newConnection.model('deleted-users', users);
-      newConnection.model('phones', phones);
-      newConnection.model('adresses', adresses);
-      newConnection.model('sneakers_30-07-2020', sneakers);
-      newConnection.model('errors', errors);
-      newConnection.model('cards', cards);
-      newConnection.model('adComplaints', adComplaints);
-      newConnection.model('ads', ads);
-      newConnection.model('orders', orders);
-      newConnection.model('bankAccounts', bankAccounts);
-      newConnection.model('multiOrders', multiOrders);
-      newConnection.model('offers', offers);
-      newConnection.model('configurations', configurations);
-      newConnection.model('payments', payments);
-      newConnection.model('transactions', transactions);
-      newConnection.model('comments', comments);
-      newConnection.model('banks', banks);
-      newConnection.model('tickets', tickets);
-      newConnection.model('countries', countries);
-      newConnection.model('newsletter-users', newsletterUsers);
       return newConnection;
     }
     console.log('=> using existing database connection');
